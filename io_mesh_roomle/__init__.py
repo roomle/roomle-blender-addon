56ca0630ac85429e6a4ab82a5a74800323322b4c
Andreas Atteneder <andreas.atteneder@roomle.com>
2018-08-30 11:57:57 +0200

    Updating release version to 0.3.0

--- io_mesh_roomle/__init__.py
+++ io_mesh_roomle/__init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Roomle Configurator Script",
     "author": "Andreas Atteneder",
-    "version": (0, 2, 1),
+    "version": (0, 3, 0),
     "blender": (2, 79, 0),
     "location": "File > Import-Export > Roomle",
     "description": "Export Roomle Configurator Script",
