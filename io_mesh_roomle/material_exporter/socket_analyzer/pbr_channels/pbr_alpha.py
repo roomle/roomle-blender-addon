0273df29343df12611b6e5da40a72ba9f1a924b9
Clemens Holleis <clemens.holleis@roomle.com>
2024-06-10 16:12:31 +0200

    wip: added alpha support

--- io_mesh_roomle/material_exporter/socket_analyzer/pbr_channels/pbr_alpha.py
+++ io_mesh_roomle/material_exporter/socket_analyzer/pbr_channels/pbr_alpha.py
@@ -6,7 +6,6 @@ from io_mesh_roomle.material_exporter.socket_analyzer import PBR_ChannelTester
 from io_mesh_roomle.material_exporter._exporter import PBR_Channel
 
 
-
 class alpha(PBR_ChannelTester):
     # TODO: image alpha?
     def __init__(self, material: bpy.types.Material) -> None:
@@ -17,6 +16,15 @@ class alpha(PBR_ChannelTester):
     def check_no_texture(self) -> Union[PBR_Channel, None]:
         if self.socket.is_linked:
             return
-        return PBR_Channel(
-            default_value=self.def_val
-        )
+        return PBR_Channel(default_value=self.def_val)
+
+    def check_image_attached_via_math(self) -> Union[PBR_Channel, None]:
+        if not self.socket.is_linked:
+            return
+
+        n = self.origin(self.socket)
+
+        if not isinstance(n, bpy.types.ShaderNodeMath):
+            return
+
+        return PBR_Channel(default_value=n.inputs[1].default_value)
